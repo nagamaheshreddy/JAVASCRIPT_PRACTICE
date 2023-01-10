29ae2328497b402dc5778ab0d8b5f52d5d22253d
Naga Maheswara Reddy <naga.maheswara.57@gmail.com>
2023-01-10 12:17:52 +0530

    First commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+// const age=15;
+
+// if(age>18){
+//     console.log("you are eligible to vote 😃😃 ");
+// }else{
+//     let yearsLeft=18-age;
+//     console.log(`you are not eligible to vote wait for another ${yearsLeft} years`);
+// }
+
+// const year='1998';
+// console.log(Number(year)+18);
+// console.log(typeof year);
+
+
+
+const friends=['mahesh','jeevan','ramesh'];
+console.log(friends.length);
+friends.push('satish');
+friends.pop();
+friends.unshift('nani');
+friends.shift();
+console.log(friends.indexOf('mahesh'));
